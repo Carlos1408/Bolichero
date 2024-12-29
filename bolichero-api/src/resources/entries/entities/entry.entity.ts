5fc260f00bf54848b5a64668e3ff2eeff51f0019
muebleria <branko.ortuno@hotmail.com>
2024-12-28 23:40:15 -0400

    implementacion del crud de entries

---
+++ bolichero-api/src/resources/entries/entities/entry.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Customer } from '../../customers/entities/customer.entity'; // Ajusta la ruta según tu proyecto
+import { Club } from '../../clubs/entities/club.entity'; // Ajusta la ruta según tu proyec
+
+@Entity()
+export class Entry {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @Column({ type: 'datetime' })
+    datetime: Date;
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+  
+    @ManyToOne(() => Customer, (customer) => customer.entries, { onDelete: 'CASCADE' })
+    customer: Customer;
+  
+    @ManyToOne(() => Club, (club) => club.entries, { onDelete: 'CASCADE' })
+    club: Club;
+}
